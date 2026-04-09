e21a8355b19ff0563cca1235076655801068094f
omersusin <tekgercek73@gmail.com>
2026-04-09 05:15:30 +0300

    fix: use android base theme, add material dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,6 +70,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation("com.google.android.material:material:1.12.0")
     implementation(libs.androidx.material.icons)
 
     implementation(libs.hilt.android)
